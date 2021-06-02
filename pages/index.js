fc77889b695d155c38baa34900e9911e3850995b
Nathalia Laudano <nathalialaudano@crazytechlabs.com.br>
2021-06-01 21:22:09 -0300

    added bootstrap to html - movedjs script to index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+var tanque = 40;
+var caminhoComGasolina = 480;
+var consumoDeGasolina = caminhoComGasolina / tanque;
+
+document.write("O consumo de gasolina é: " + consumoDeGasolina + "km/L");
+
+var caminhoComAlcool = 300;
+var consumoDeAlcool = caminhoComAlcool / tanque;
+
+document.write("<br> O consumo de álcool é: " + consumoDeAlcool + "km/L");
