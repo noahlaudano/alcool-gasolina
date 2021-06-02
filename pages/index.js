ae62c70a813c72e5c939b875ad3f58860a3d928a
Nathalia Laudano <nathalialaudano@crazytechlabs.com.br>
2021-06-01 21:59:53 -0300

    first configuration of internal calculator

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import '.style.css'
+
 var tanque = 40;
 var caminhoComGasolina = 480;
 var consumoDeGasolina = caminhoComGasolina / tanque;
@@ -8,3 +12,27 @@ var caminhoComAlcool = 300;
 var consumoDeAlcool = caminhoComAlcool / tanque;
 
 document.write("<br> O consumo de álcool é: " + consumoDeAlcool + "km/L");
+
+var valorEtanol = 1;
+var valorGasolina = 6.99;
+
+var desempenhoEtanol = consumoDeAlcool / consumoDeGasolina;
+var valorNaBomba = valorEtanol / valorGasolina;
+
+document.write(
+  "<br> O desempenho do carro com etanol é de " +
+    Math.round(desempenhoEtanol * 100) +
+    "%."
+);
+
+document.write(
+  "<br> A relação do preço etanol/gasolina na bomba é de " +
+    Math.round(valorNaBomba * 100) +
+    "%."
+);
+
+if (valorNaBomba <= desempenhoEtanol) {
+  document.write("<br> Vale mais a pena o etanol!");
+} else {
+  document.write("<br> Vale mais a pena a gasolina!");
+}
