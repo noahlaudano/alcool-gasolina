b9060600ef596c17dd9e771e5811c21507111b41
Nathalia Laudano <nathalialaudano@crazytechlabs.com.br>
2021-06-02 21:42:34 -0300

    to push

--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,26 @@
 // import ReactDOM from 'react-dom'
 // import '.style.css'
 
-var tanque = 40;
-var caminhoComGasolina = 480;
-var consumoDeGasolina = caminhoComGasolina / tanque;
+let consumoDeGasolina = document.getElementById("km-gas");
 
-document.write("O consumo de gasolina é: " + consumoDeGasolina + "km/L");
+// document.write("O consumo de gasolina é: " + consumoDeGasolina + "km/L");
 
-var caminhoComAlcool = 300;
-var consumoDeAlcool = caminhoComAlcool / tanque;
+let consumoDeAlcool = document.getElementById("km-alc");
 
-document.write("<br> O consumo de álcool é: " + consumoDeAlcool + "km/L");
+// document.write("<br> O consumo de álcool é: " + consumoDeAlcool + "km/L");
 
-var valorEtanol = 1;
-var valorGasolina = 6.99;
+let valorEtanol = document.getElementById("value-alc");
+let valorGasolina = document.getElementById("value-gas");
 
-var desempenhoEtanol = consumoDeAlcool / consumoDeGasolina;
-var valorNaBomba = valorEtanol / valorGasolina;
+let desempenhoEtanol = consumoDeAlcool / consumoDeGasolina;
 
-document.write(
+let valorNaBomba = valorEtanol / valorGasolina;
+
+let desempenho = Math.round(desempenhoEtanol * 100);
+
+let relacaoValor = Math.round(valorNaBomba * 100);
+
+/** document.write(
   "<br> O desempenho do carro com etanol é de " +
     Math.round(desempenhoEtanol * 100) +
     "%."
@@ -29,10 +31,13 @@ document.write(
   "<br> A relação do preço etanol/gasolina na bomba é de " +
     Math.round(valorNaBomba * 100) +
     "%."
-);
+);*/
+let answer = document.querySelector("h4");
 
-if (valorNaBomba <= desempenhoEtanol) {
-  document.write("<br> Vale mais a pena o etanol!");
-} else {
-  document.write("<br> Vale mais a pena a gasolina!");
+function submit() {
+  if (relacaoValor <= desempenho) {
+    answer.innerHTML = "Vale mais a pena o etanol!";
+  } else {
+    answer.innerHTML = "Vale mais a pena a gasolina!";
+  }
 }
